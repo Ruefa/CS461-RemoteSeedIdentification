8eea8ee09bb2207b344b635b34d635c01d452fd1
ruefa <ruef.alex@gmail.com>
2018-01-19 17:22:31 -0800

    created CameraControl.java

---
+++ App/app/src/main/java/com/tests/alex/remoteseedidentification/CameraControl.java
@@ -0,0 +1,16 @@
+package com.tests.alex.remoteseedidentification;
+
+/**
+ * Created by Alex on 1/19/2018.
+ */
+
+public class CameraControl {
+
+    public static boolean safeCameraOpen(){
+        boolean camOpened = false;
+
+
+
+        return camOpened;
+    }
+}
