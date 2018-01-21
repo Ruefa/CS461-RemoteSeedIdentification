8bc215d3eaeed0e8e9b9b48fe596166847f6c7d1
ruefa <ruef.alex@gmail.com>
2018-01-20 17:59:36 -0800

    Initialize Camera in CameraView

--- App/app/src/main/java/com/tests/alex/remoteseedidentification/CameraView.java
+++ App/app/src/main/java/com/tests/alex/remoteseedidentification/CameraView.java
@@ -1,9 +1,13 @@
 package com.tests.alex.remoteseedidentification;
 
 import android.content.Context;
+import android.hardware.Camera;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
 import android.view.SurfaceHolder;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Alex on 1/20/2018.
@@ -11,8 +15,10 @@ import android.view.SurfaceHolder;
 
 public class CameraView extends ViewGroup implements SurfaceHolder.Callback {
 
-    SurfaceView mSurfaceView;
-    SurfaceHolder mHolder;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private List<Camera.Size> mSupportedPreviewSizes;
 
     CameraView(Context context){
         super(context);
@@ -24,6 +30,29 @@ public class CameraView extends ViewGroup implements SurfaceHolder.Callback {
         mHolder.addCallback(this);
     }
 
+
+    public void setCamera(Camera camera){
+        if(mCamera == camera) { return; }
+
+        stopPreviewAndFreeCamera();
+
+        mCamera = camera;
+
+        if(mCamera != null){
+            List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+            mSupportedPreviewSizes = localSizes;
+            requestLayout();
+
+            try{
+                mCamera.setPreviewDisplay(mHolder);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            mCamera.startPreview();
+        }
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
 
@@ -31,12 +60,17 @@ public class CameraView extends ViewGroup implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
+        requestLayout();
+        mCamera.setParameters(parameters);
 
+        mCamera.startPreview();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
+        
     }
 
     @Override
