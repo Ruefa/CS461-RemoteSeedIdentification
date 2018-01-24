0de105e88f4773f8f254a1b170789cd7df6e2994
ruefa <ruef.alex@gmail.com>
2018-01-23 19:13:58 -0800

    working pullout menu with misplaced text

--- App/app/src/main/java/com/capstone/remoteseedidentification/MainActivity.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/MainActivity.java
@@ -1,21 +1,25 @@
 package com.capstone.remoteseedidentification;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Camera mCamera;
     private CameraView mCameraView;
+    private String[] mNavData;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         mCameraView = new CameraView(this, mCamera);
         FrameLayout frameLayout = findViewById(R.id.cam_view);
         frameLayout.addView(mCameraView);
+
+        initNavigation();
     }
 
     private Camera getCameraInstance(){
@@ -46,5 +52,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return cam;
+    }
+
+    private void initNavigation(){
+        mNavData = dummyData(5);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerView = findViewById(R.id.navigation_list_view);
+
+        mDrawerView.setAdapter(new ArrayAdapter<String>(this, R.layout.nav_text_view, mNavData));
+    }
+
+    private String[] dummyData(int numData){
+        String[] data = new String[numData];
+
+        for(int i=0; i<data.length; i++){
+            data[i] = String.valueOf(i+1);
+        }
+
+        return data;
     }
 }
