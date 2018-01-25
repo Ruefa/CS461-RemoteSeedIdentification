df0e6686594074c3123b216829b473377194d7cb
ruefa <ruef.alex@gmail.com>
2018-01-24 17:49:38 -0800

    added activity switching logic to main activity

--- App/app/src/main/java/com/capstone/remoteseedidentification/MainActivity.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/MainActivity.java
@@ -1,6 +1,7 @@
 package com.capstone.remoteseedidentification;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.support.v4.app.ActivityCompat;
@@ -8,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
@@ -72,5 +74,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return data;
+    }
+
+    private void goLogin(View view){
+        Intent intent = new Intent(this, LoginController.class);
+
+        startActivity(intent);
     }
 }
