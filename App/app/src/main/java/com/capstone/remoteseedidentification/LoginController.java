7cf5a25a63cc2f363fdba922dfc4c5c0d0a091a3
ruefa <ruef.alex@gmail.com>
2018-02-01 17:19:02 -0800

    added skeleton code to send user to mainActivity after loging in

--- App/app/src/main/java/com/capstone/remoteseedidentification/LoginController.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/LoginController.java
@@ -1,7 +1,9 @@
 package com.capstone.remoteseedidentification;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginController extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class LoginController extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void doLogin(View v){
+        boolean loginSuccess = true;
+
+        if(loginSuccess) {
+            goMain();
+        }
+    }
+
+    private void goMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
     }
 }
