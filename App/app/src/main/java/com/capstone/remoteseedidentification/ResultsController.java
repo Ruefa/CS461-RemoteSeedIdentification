40bb667c672d14a1c4e18d26df53cffca8496553
ruefa <ruef.alex@gmail.com>
2018-02-02 17:48:47 -0800

    created results activty with controller

---
+++ App/app/src/main/java/com/capstone/remoteseedidentification/ResultsController.java
@@ -0,0 +1,13 @@
+package com.capstone.remoteseedidentification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
