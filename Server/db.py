288514e42416baa731dbf34bedd3fab4515bf9fc
Quanah Green <quanahgreen@gmail.com>
2018-02-15 22:21:22 -0800

    Added preliminary database code

---
+++ Server/db.py
@@ -0,0 +1,48 @@
+from pony.orm import *
+
+
+def dbSetup():
+    db = Database()
+
+    class Account(db.Entity):
+        username = PrimaryKey(str)
+        password = Required(str)
+        reports = Set('Report')
+
+    class Report(db.Entity):
+        results = Required(str)
+        owner = Required(Account)
+
+    db.bind(provider='sqlite', filename=':memory:')
+    db.generate_mapping(create_tables=True)
+
+    return db
+
+@db_session
+def makeLogin(username, password):
+    # Check if username is available
+    if Account.exists(username=username):
+        return False
+
+    #TODO Probably shouldn't store pword as plaintxt
+   
+   # If it is, make account
+    Account(username=username, password=password)
+    return True
+
+@db_session
+def checkLogin(username, password):
+    return Account.exists(username=username, password=password)
+
+@db_session
+def addReport(username, results):
+    Report(results=results, owner=Account[username])
+
+@db_session
+def getReportList(username):
+    return select(x.id for x in Account[username].reports)[:]
+
+@db_session
+def getReport(username, report):
+    return select(x for x in Account[username].reports if x.id == report)
+
