e09f6771d75566124d706746a87880a84f94a9f1
ruefa <ruef.alex@gmail.com>
2018-02-22 20:12:23 -0800

    created async task to run ServerUtils in

---
+++ App/app/src/main/java/com/capstone/remoteseedidentification/ServerAsyncTask.java
@@ -0,0 +1,33 @@
+package com.capstone.remoteseedidentification;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+/**
+ * Created by Alex on 2/22/2018.
+ */
+
+public class ServerAsyncTask extends AsyncTask<String, String, ServerUtils> {
+
+    private ServerUtils mServer;
+
+    //constructor currently takes no args
+    public ServerAsyncTask(){}
+
+    @Override
+    protected ServerUtils doInBackground(String... strings) {
+
+        try{
+            mServer = new ServerUtils(new ServerUtils.MessageCallback() {
+                @Override
+                public void callbackMessageReceiver(String message) {
+                    Log.d("Server message: ", message);
+                }
+            });
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        mServer.openSocket();
+        return null;
+    }
+}
