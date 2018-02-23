f23fe73f326da320a72c12530a6a1b58475ff70c
ruefa <ruef.alex@gmail.com>
2018-02-22 19:26:12 -0800

    started on open socket implementation

--- App/app/src/main/java/ServerUtils.java
+++ App/app/src/main/java/ServerUtils.java
@@ -1,9 +1,38 @@
+import android.util.Log;
+
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * Created by Alex on 2/22/2018.
  */
 
 public class ServerUtils {
 
-    public static final String SERVER_IP = "";
-    public static final int SERVER_PORT = 5777;
+    private static final String SERVER_IP = "";
+    private static final int SERVER_PORT = 5777;
+
+    private PrintWriter mOutBuffer;
+    private MessageCallback mMessageCallback;
+
+    public ServerUtils(MessageCallback listener){
+        mMessageCallback = listener;
+    }
+
+    public void openSocket() {
+
+        try {
+            InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+            Socket socket = new Socket(serverAddr, SERVER_PORT);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public interface MessageCallback {
+
+        public void callbackMessageReceiver(String message);
+    }
 }
