bd1d84c6d7f0ab52e00d9ddab68c35f14d3d5fd2
ruefa <ruef.alex@gmail.com>
2018-02-23 20:02:11 -0800

    setup login controller to launch async task for socket connection.

--- App/app/src/main/java/com/capstone/remoteseedidentification/LoginController.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/LoginController.java
@@ -1,6 +1,10 @@
 package com.capstone.remoteseedidentification;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +15,20 @@ public class LoginController extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 0);
+        }
     }
 
     public void doLogin(View v){
         boolean loginSuccess = true;
+
+        //open socket with server
+        ServerAsyncTask task = new ServerAsyncTask();
+        task.execute("");
+        //task.sendMessage("test");
+
 
         if(loginSuccess) {
             goMain();
