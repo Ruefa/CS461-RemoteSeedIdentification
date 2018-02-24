90770dd4c7d0ce836b43fd4c3400d57e58181a40
ruefa <ruef.alex@gmail.com>
2018-02-23 20:01:44 -0800

    added sendMessage method, just testing

--- App/app/src/main/java/com/capstone/remoteseedidentification/ServerAsyncTask.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/ServerAsyncTask.java
@@ -1,5 +1,6 @@
 package com.capstone.remoteseedidentification;
 
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -12,12 +13,13 @@ public class ServerAsyncTask extends AsyncTask<String, String, ServerUtils> {
     private ServerUtils mServer;
 
     //constructor currently takes no args
-    public ServerAsyncTask(){}
+    //public ServerAsyncTask(){}
 
     @Override
     protected ServerUtils doInBackground(String... strings) {
 
         try{
+            Log.d("Server: ", "about to begin");
             mServer = new ServerUtils(new ServerUtils.MessageCallback() {
                 @Override
                 public void callbackMessageReceiver(String message) {
@@ -29,5 +31,9 @@ public class ServerAsyncTask extends AsyncTask<String, String, ServerUtils> {
         }
         mServer.openSocket();
         return null;
+    }
+
+    public void sendMessage(String message){
+        mServer.sendMessage(message);
     }
 }
