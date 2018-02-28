bc65d4ffbb6e9821aee133fae51d8a12bc9e3ec3
ruefa <ruef.alex@gmail.com>
2018-02-27 19:38:42 -0800

    SocketService working to communicate with server

--- App/app/src/main/java/com/capstone/remoteseedidentification/SocketService.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/SocketService.java
@@ -3,6 +3,7 @@ package com.capstone.remoteseedidentification;
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by Alex on 2/27/2018.
@@ -12,12 +13,25 @@ public class SocketService extends IntentService {
 
     public static final String WORKER_THREAD_NAME = "socket_worker";
 
+    private ServerUtils mServer;
+
     public SocketService() {
         super(WORKER_THREAD_NAME);
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-
+        try{
+            Log.d("Server: ", "about to begin");
+            mServer = new ServerUtils(new ServerUtils.MessageCallback() {
+                @Override
+                public void callbackMessageReceiver(String message) {
+                    Log.d("Server message: ", message);
+                }
+            });
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        mServer.openSocket();
     }
 }
