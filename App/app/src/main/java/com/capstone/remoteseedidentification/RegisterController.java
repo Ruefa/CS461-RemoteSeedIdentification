a79b26975bd522651d5d338350894418f1d6ebed
ruefa <ruef.alex@gmail.com>
2018-03-06 17:16:01 -0800

    added handling for error text and password confirm

--- App/app/src/main/java/com/capstone/remoteseedidentification/RegisterController.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/RegisterController.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterController extends AppCompatActivity {
 
     private static final String TAG = "RegisterController";
 
-    private EditText etUser, etPass;
+    private EditText etUser, etPass, etPassConfirm;
+    private TextView tvError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +21,24 @@ public class RegisterController extends AppCompatActivity {
 
         etUser = findViewById(R.id.et_register_username);
         etPass = findViewById(R.id.et_register_pass);
+        etPassConfirm = findViewById(R.id.et_register_pass_confirm);
+        tvError = findViewById(R.id.tv_register_error);
     }
 
     public void doRegister(View v){
         final String message;
 
-        message = "a" + etUser.getText().toString() + "@" + etPass.getText().toString();
+        tvError.setVisibility(View.INVISIBLE);
 
-        ServerAsyncTask asyncTask = new ServerAsyncTask(mCallback);
-        asyncTask.execute(message);
+        if(etPass.getText().toString().equals(etPassConfirm.getText().toString())) {
+            message = "a" + etUser.getText().toString() + "@" + etPass.getText().toString();
+
+            ServerAsyncTask asyncTask = new ServerAsyncTask(mCallback);
+            asyncTask.execute(message);
+        }else{
+            tvError.setText(R.string.register_error_mismatch);
+            tvError.setVisibility(View.VISIBLE);
+        }
     }
 
     private ServerUtils.MessageCallback mCallback = new ServerUtils.MessageCallback() {
