c94f9ffee3b0556f043631bd8df70af778732f00
taklae <taklae@oregonstate.edu>
2018-04-06 09:37:15 -0700

    Added non-maximum supression

--- Classifier/sample_analysis.py
+++ Classifier/sample_analysis.py
@@ -1,3 +1,27 @@
+#*************************************************************************************************#
+#                                                                                                 #
+#   File: sample_analysis.py                                                                      #
+#   Author: Ethan Takla                                                                           #
+#   Last modified: 4/5/2018                                                                       #
+#   Description: This file contains all the tools to localize and predict the species of          #
+#                an arbitrary amount of seeds in an image. The large sample image is partitioned  #
+#                into many small slices, which are each individually analyzed by the classifier.  #
+#                A single-shot multibox detector (SSD) is used along with a VGG-16 base network   #
+#                to run perform classification. Slice deltas are 1/3 of the slice width so seeds  #
+#                caught on the edges will be guaranteed to appear wholly in at least image.       #
+#                Probability-based non-maximal suppression is used to merge any redundant         #
+#                detections.                                                                      #
+#   Inputs:                                                                                       #
+#           -img        Path to the image to be analzyed                                          #
+#           -weights    Path to the weights file for the classifier                               #
+#   Outputs:                                                                                      #
+#           results.png     A .PNG image of all of the image overlaid with the seed detections    #
+#           statistics.dat  A text-based file containing the possible species and their           #
+#                           composition percentages                                               #
+#                                                                                                 #
+#*************************************************************************************************#
+
+
 import os
 import sys
 import torch
@@ -18,9 +42,14 @@ from ssd import build_ssd
 import math
 import argparse
 
-
+# Size of center-based inclusion square. BBox's are included if their centroids fit in this square. This is used
+# mainly to eliminate seed detections that touch the edges of the slices
 INCLUSION_WINDOW = 0.75
 
+# Amount of overlap required for BBox's to be considered bounding the same object
+OVERLAP_THRESHOLD = 0.8
+
+# Create an argument parser for weights and image file
 parser = argparse.ArgumentParser(description='Seed sample analzyer')
 parser.add_argument('image', metavar='img', type=str, nargs='+',
                    help='path to the sample image')
@@ -31,9 +60,11 @@ module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
 
+# Set up cuda for floating ops
 if torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
+# The possible species that can be detected
 species_names = ['prg', 'tf']
 
 # Chop up an sample image into smaller, overlapping windows
@@ -132,6 +163,63 @@ def analyze_sample(image, net, slice_shape):
 
     return predictions
 
+
+# Non-maximal supression from Adrian Rosebrock, based on Malisiewicz et al.
+def non_max_suppression_fast(boxes, overlapThresh):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                               np.where(overlap > overlapThresh)[0])))
+
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    return boxes[pick].astype("int")
 
 def save_predicitons(image, predictions):
 
