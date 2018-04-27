fba0e33c69011ef48ba7ff6ef4c13706f162061f
Quanah Green <quanahgreen@gmail.com>
2018-04-27 02:13:28 -0700

    lowercase usernames

--- Server/db.py
+++ Server/db.py
@@ -23,11 +23,13 @@ class Report(db.Entity):
     owner = Required(Account)
 
 def dbInit(dbDirectory = dbDirectory):
+    global db
     db.bind(provider='sqlite', filename=str(dbDirectory/'RemoteSeedDB.sqlite'), create_db=True)
     db.generate_mapping(create_tables=True)
 
 @db_session
 def newAccount(username, password):
+    username = username.lower()
     # Check if username is available
     if Account.exists(username=username):
         return False
@@ -38,6 +40,7 @@ def newAccount(username, password):
 
 @db_session
 def login(username, password, newToken = True):
+    username = username.lower()
     account = Account.get(username=username)
     if account and checkPassword(password, account.password): # Username and Password are correct
         if newToken:
@@ -50,6 +53,7 @@ def login(username, password, newToken = True):
 
 @db_session
 def checkToken(username, token):
+    username = username.lower()
     #TODO Exception handling
     account = Account.get(username=username)
     if account and token == account.sessionToken:
@@ -58,6 +62,7 @@ def checkToken(username, token):
 
 @db_session
 def logout(username):
+    username = username.lower()
     #TODO Exception handling
     account = Account.get(username=username)
     if account:
@@ -65,12 +70,14 @@ def logout(username):
 
 @db_session
 def changePassword(username, newPass):
+    username = username.lower()
     account = Account.get(username=username)
     if account:
         account.password = hashPassword(newPass)
 
 @db_session
 def newPassword(username):
+    username = username.lower()
     account = Account.get(username=username)
     if account:
         newPass = makePassword
@@ -80,6 +87,7 @@ def newPassword(username):
 
 @db_session
 def newReport(username, sourceImg='', resultsImg='', results=''):
+    username = username.lower()
     r = Report(owner=Account[username], sourceImg=sourceImg, resultsImg=resultsImg, results=results)
     commit()
     return r.id
@@ -93,6 +101,7 @@ def updateReport(reportId, sourceImg='', resultsImg='', results=''):
 
 @db_session
 def getReportList(username):
+    username = username.lower()
     return select(x.id for x in Report if x.owner == Account[username])[:]
 
 @db_session
@@ -109,10 +118,12 @@ def deleteReport(reportID):
 
 @db_session
 def deleteAllReports(username):
+    username = username.lower()
     delete(r for r in Report if r.owner == username)
 
 @db_session
 def deleteAccount(username):
+    username = username.lower()
     acct = Account.get(username=username)
     if acct:
         acct.delete()
