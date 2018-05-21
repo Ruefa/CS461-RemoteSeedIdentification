61db8f6b88c36bad9099651b7aea797df47d91e7
ruefa <ruef.alex@gmail.com>
2018-05-20 19:53:50 -0700

    removerd unused imports

--- App/app/src/main/java/com/capstone/remoteseedidentification/ResultsController.java
+++ App/app/src/main/java/com/capstone/remoteseedidentification/ResultsController.java
@@ -1,14 +1,13 @@
 package com.capstone.remoteseedidentification;
 
-import android.app.IntentService;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,14 +15,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
 
 import java.util.ArrayList;
 import java.util.Arrays;
